c2aa4b87439ee48164542cd065249ab5d91cf65f
philipangeloang <philipangeloang1@gmail.com>
2023-11-18 03:01:30 +0800

    Added all function operations

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import { KeyExpansion } from "@/data/operations";
 import Image from "next/image";
 
 export default function Home() {
-  keyExpansion("0f1571c947d9e8590cb7add6af7f6798");
+  KeyExpansion("0f1571c947d9e8590cb7add6af7f6798");
   return (
     <>
       <div>Test</div>

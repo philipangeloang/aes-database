f06d8dd12291839c06b9841deebce234bd84f574
philipangeloang <philipangeloang1@gmail.com>
2023-11-19 02:29:10 +0800

    AddRoundKey working perfectly

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,12 @@
-import { KeyExpansion } from "@/data/operations";
+import { KeyExpansion, AESEncrpty } from "@/data/operations";
 import Image from "next/image";
 
 export default function Home() {
-  KeyExpansion("0f1571c947d9e8590cb7add6af7f6798");
+  const expandedKey = KeyExpansion("0f1571c947d9e8590cb7add6af7f6798");
+  const cipherText = AESEncrpty(
+    "0123456789abcdeffedcba9876543210",
+    expandedKey
+  );
   return (
     <>
       <div>Test</div>
--- src/data/operations.js
+++ src/data/operations.js
@@ -1,16 +1,16 @@
 import { encrpytionData, cipherKeys, sBox, Rcon } from "./data";
 
 export function KeyExpansion(key) {
-  let inputKey = key.match(/.{1,2}/g);
+  let inputKey = key.match(/.{1,2}/g); // splitting input key per group of 2
   let hexKeys = [];
 
-  // Converting string to hexadecimal
+  // Converting string to hexadecimal | ae -> 0xae
   for (let i = 0; i < inputKey.length; i++) {
     const numericValue = parseInt("0x" + inputKey[i], 16);
     hexKeys.push("0x" + numericValue.toString(16));
   }
 
-  // Grouping the hexadecimal keys firstly into 4 per word
+  // Grouping the hexadecimal keys firstly into 4 per word | creation of nesting
   function createGroups(arr, numGroups) {
     const perGroup = Math.ceil(arr.length / numGroups);
     return new Array(numGroups)
@@ -34,9 +34,9 @@ export function KeyExpansion(key) {
     // Iterate over each byte in the row and substitute using the S-box
     for (let i = 0; i < inputRow.length; i++) {
       const byte = inputRow[i];
-      const row = (byte >>> 4) & 0x0f;
-      const col = byte & 0x0f;
-      inputRow[i] = "0x" + sBox[row * 16 + col].toString(16);
+      const row = (byte >>> 4) & 0x0f; //0x[a]e -> extracting upper bit
+      const col = byte & 0x0f; //0xa[e] -> extracting lower bit
+      inputRow[i] = "0x" + sBox[row * 16 + col].toString(16); // getting equivalent value in sbox matrix then convert to hexadecimal
     }
     return inputRow;
   }
@@ -45,8 +45,10 @@ export function KeyExpansion(key) {
   function xorRcon(input, round) {
     for (let i = 0; i < 4; i++) {
       if (i == 0) {
+        // the only important XOR (0x[01]00000)
         input[i] = "0x" + (input[i] ^ Rcon[round]).toString(16);
       } else {
+        // all else is XORed to 00
         input[i] = "0x" + (input[i] ^ 0x00).toString(16);
       }
     }
@@ -57,25 +59,45 @@ export function KeyExpansion(key) {
   function xor(input1, input2) {
     let newArr = [];
     for (let i = 0; i < 4; i++) {
+      // XOR byte per byte
       newArr.push("0x" + (input1[i] ^ input2[i]).toString(16));
     }
     return newArr;
   }
 
-  let expandedKeys = createGroups(hexKeys, 4);
+  let expandedKeys = createGroups(hexKeys, 4); // grouping keys by 4 (w0, w1, w2, w3, ...)
 
   for (let i = 4; i < 44; i++) {
-    let temp = [...expandedKeys[i - 1]];
+    let temp = [...expandedKeys[i - 1]]; // ...expandedKeys is spread to avoid referencing to one point. This is done to make new reference
     if (i % 4 === 0) {
-      temp = xorRcon(substituteRow(rotWord(temp)), i / 4 - 1);
+      // this will only happen per multiple of 4 where the confusion diffusion is added
+      temp = xorRcon(substituteRow(rotWord(temp)), i / 4 - 1); // i / 4 - 1 to start with round 0. XOR with confusion and diffusion
     }
-    expandedKeys[i] = xor(expandedKeys[i - 4], temp);
+    expandedKeys[i] = xor(expandedKeys[i - 4], temp); // if not multiple of 4, will XOR past and current byte
   }
 
-  console.log(expandedKeys);
+  return expandedKeys; // keys are returned as a word (w0, w1, w2, ... w44)
+  // console.log(expandedKeys);
 }
 
 export function AESEncrpty(text, key) {
+  let inputText = text.match(/.{1,2}/g);
+  let hexText = [];
+
+  // Converting string to hexadecimal | ae -> 0xae
+  for (let i = 0; i < inputText.length; i++) {
+    const numericValue = parseInt("0x" + inputText[i], 16);
+    hexText.push("0x" + numericValue.toString(16));
+  }
+
+  // Grouping the text input to create a 4 by 4 state matrix
+  function createGroups(arr, numGroups) {
+    const perGroup = Math.ceil(arr.length / numGroups);
+    return new Array(numGroups)
+      .fill("")
+      .map((_, i) => arr.slice(i * perGroup, (i + 1) * perGroup));
+  }
+
   function SubBytes(state) {
     // Assuming state is a 2D array representing the state matrix
     for (let i = 0; i < 4; i++) {
@@ -142,8 +164,20 @@ export function AESEncrpty(text, key) {
   function AddRoundKey(state, key) {
     for (let i = 0; i < 4; ++i) {
       for (let j = 0; j < 4; ++j) {
-        state[i][j] ^= key[round * Nb * 4 + i * Nb + j];
+        state[i][j] = "0x" + (state[i][j] ^ key[i][j]).toString(16); // XORing byte per byte state ^ key
       }
+    }
+    return state;
+  }
+
+  let state = createGroups(hexText, 4);
+  let keys = createGroups(key, 11);
+
+  // Start of the Encryption Algorithm
+  for (let i = 0; i <= 10; i++) {
+    if (i == 0) {
+      // round 0 initial addroundkey
+      state = AddRoundKey(state, keys[i]);
     }
   }
 }

3a0be13183ae4e61c571717a9c3cd623682c9c82
Philip <philipangeloang1@gmail.com>
2023-11-24 14:35:12 +0800

    Ongoing hiplip key expansion

--- src/aes/aes_key_expansion/hiplip_key_expansion.js
+++ src/aes/aes_key_expansion/hiplip_key_expansion.js
@@ -23,8 +23,75 @@ export function HiplipKeyExpansion(key, n) {
       .map((_, i) => arr.slice(i * perGroup, (i + 1) * perGroup));
   }
 
+  function combineHextoBin(state) {
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        state[i][j] = parseInt(state[i][j], 16).toString(2).padEnd(8, "0");
+      }
+    }
+
+    for (let i = 0; i < 4; i++) {
+      state[i] = state[i].join("");
+    }
+
+    return state.join("");
+  }
+
+  function binaryStringToArray(binaryString) {
+    return binaryString.split("").map(Number);
+  }
+
+  function arrayToBinaryString(bitArray) {
+    return bitArray.join("");
+  }
+
+  function leftShift(array, shiftAmount) {
+    return array.slice(shiftAmount).concat(new Array(shiftAmount).fill(0));
+  }
+
+  function rightShift(array, shiftAmount) {
+    return new Array(shiftAmount)
+      .fill(0)
+      .concat(array.slice(0, array.length - shiftAmount));
+  }
+
   let mainKey = createGroups(hexKeys, 4); // grouping keys by 4 (w0, w1, w2, w3, ...)
   let subMainKey = SubBytes(mainKey);
+
+  let testKey = combineHextoBin(mainKey);
+  testKey = binaryStringToArray(testKey);
+  console.log(testKey);
+  testKey = leftShift(testKey, 2);
+  console.log(testKey);
+  testKey = arrayToBinaryString(testKey);
+  console.log(testKey);
+  testKey = createGroups(testKey, 16);
+  testKey = createGroups(testKey, 4);
+  console.log(testKey);
+  testKey[0][0] = "0x" + parseInt(testKey[0][0], 16);
+  console.log(testKey);
+  console.log("DOWN");
+  console.log(parseInt("000001010", 2).toString(16));
+
+  // ith key generation
+  for (let i = 0; i < n; i++) {
+    let tempKey = combineHextoBin(mainKey);
+    tempKey = binaryStringToArray(tempKey);
+    if ((tempKey & 1) === 1) {
+      tempKey = leftShift(tempKey, i + 1);
+    } else {
+      tempKey = rightShift(tempKey, i + 1);
+    }
+    tempKey = arrayToBinaryString(tempKey);
+    tempKey = createGroups(tempKey, 16);
+    tempKey = createGroups(tempKey, 4);
+
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        tempKey[i][j] = "0x" + parseInt(tempKey[i][j], 2);
+      }
+    }
+  }
 
   return expandedKeys; // keys are returned as a word (w0, w1, w2, ... w44)
 }
